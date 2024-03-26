e6009886a7c95a0db8894c992474e5eb9ff2739d
OfomiMatthew <62011691+OfomiMatthew@users.noreply.github.com>
2024-03-26 21:59:10 +0000

    using flask jinja template for dynamic rendering

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+jobs = [{
+    'id': 1,
+    'title': 'Data analyst',
+    'location': "Abuja",
+    'salary': 'Naira 200,000'
+},
+				{
+						'id': 2,
+						'title': 'Data engineer',
+						'location': "Lagos",
+						# 'salary': 'Naira 300,000'
+				},
+
+				{
+						'id': 3,
+						'title': 'Software developer',
+						'location': "Ibadan",
+						'salary': 'Naira 200,000'
+				},
+				{
+						'id': 4,
+						'title': 'Cloud engineer',
+						'location': "PH City",
+						'salary': 'Naira 250,000'
+				},
+			 
+			 ]
 
 
 @app.route('/')
 def hello_world():
-	return render_template('home.html')
+	return render_template('home.html',jobs=jobs,company="Matt-Dev")
 
 
 if __name__ == "__main__":
